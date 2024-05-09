514df625457cd32e629bd6df3c564a4d5220df86
KODIYARASU01 <kodiyarasu02gmail.com>
2024-05-09 11:47:41 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import NewCardDesign6 from './Component/NewCardDesign6';
+
+const App = () => {
+  return (
+    <div>
+<NewCardDesign6/>
+    </div>
+  )
+}
+
+export default App;
